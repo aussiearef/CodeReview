06b72807eb656579cd0fbf85a46d2165066035a1
aussiearef <arefkr@gmail.com>
2024-02-13 10:08:25 +1100

    Added CodeReviewExample

---
+++ CodeReviewExample/CodeReviewExample/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+ * This code is written in C#
+ * To compile and run this code, download "DOTNET SDK 8"
+ * Then execute "dotnet run" in Terminal or Command Prompt
+ */
+
+Console.WriteLine("Hello, World!");
