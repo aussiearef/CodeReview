7c4c7157bd6ca7c9cbff072ceb2bfc7b3541da30
aussiearef <arefkr@gmail.com>
2024-02-13 10:05:50 +1100

    Added Good Code

---
+++ GoodCode/GoodCode/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * This code is written in C#, version 12 (.NET 8)
+ * To compile and run this code, download .NET 8 SDK
+ * Then run "dotnet run" command in Terminal or Command Prompt.
+ */
+Console.WriteLine("Hello, World!");
